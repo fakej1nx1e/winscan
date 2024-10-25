48413153d9d828ea3dbe9bae4354d65c7c4c0978
yogakumi <43279392+yogakumi0@users.noreply.github.com>
2024-10-25 18:24:45 +0200

    Add files via upload

--- start.cmd
+++ start.cmd
@@ -1,17 +1,17 @@
 @echo off
 setlocal
 
-:: Pfad zur PowerShell-Datei
+:: Path to PowerShell script
 set "PowerShellScript=winscan.ps1"
 
-:: Überprüfen, ob das Skript existiert
+:: Check if script exists
 if not exist "%PowerShellScript%" (
-    echo Das PowerShell-Skript wurde nicht gefunden: %PowerShellScript%
+    echo PowerShell script not found: %PowerShellScript%
     pause
     exit /b 1
 )
 
-:: PowerShell mit Administratorrechten ausführen
+:: Run PowerShell with administrator rights
 powershell -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%CD%\%PowerShellScript%\"' -Verb RunAs"
 
 cmd /k
